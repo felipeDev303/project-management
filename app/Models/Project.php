ef8676f3eb75a824ee893f5aa02b808f78b4b765
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-07-24 01:53:44 -0400

    Agregar modelo Project con propiedades y accesores

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,9 +2,49 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'start_date',
+        'status',
+        'responsible',
+        'monto',
+    ];
+
+    protected $casts = [
+        'start_date' => 'date',
+        'monto' => 'decimal:2',
+    ];
+
+    // Accessor para compatibilidad con la BD existente
+    public function getNombreAttribute()
+    {
+        return $this->name;
+    }
+
+    public function getFechaInicioAttribute()
+    {
+        return $this->start_date;
+    }
+
+    public function getEstadoAttribute()
+    {
+        return $this->status;
+    }
+
+    public function getResponsableAttribute()
+    {
+        return $this->responsible;
+    }
+
+    public function getMontoAttribute()
+    {
+        return $this->monto;
+    }
 }

b4abc26b462c18bd6ab1ed1ef7381943be495afd
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-07-24 03:11:46 -0400

    Eliminar rutas del controlador RouteController y simplificar la gestión de proyectos en web.php

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
-// Define las rutas de la aplicación
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProjectController;
-use App\Http\Controllers\RouteController;
 
 // Ruta de bienvenida
 Route::get('/', function () {
@@ -12,10 +10,3 @@ Route::get('/', function () {
 
 // Rutas de recursos para el controlador de proyectos
 Route::resource('projects', ProjectController::class);
-Route::get('/projects/get', [RouteController::class, 'get']);
-Route::get('/projects/create', [RouteController::class, 'create']) ->name('projects.create');
-Route::post('/projects/store', [RouteController::class, 'store']) ->name('projects.store');
-Route::get('/projects/{id}/edit', [RouteController::class, 'edit']) ->name('projects.edit');
-Route::put('/projects/{id}', [RouteController::class, 'update']) ->name('projects.update');
-Route::delete('/projects/{id}', [RouteController::class, 'destroy']) ->name('projects.destroy');
-Route::get('/projects/{id}', [RouteController::class, 'show']) ->name('projects.show');
